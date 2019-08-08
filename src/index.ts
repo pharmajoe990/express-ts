873cacad88c2a3fe2fbcb7e9e7a02f55000d90cc
Tim Roper <tim.roper@myob.com>
2019-08-08 19:34:48 +1000

    Add MongoDB backend and Mongoose for ODM

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://localhost/test", {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    // we're connected!
+});
 
 const foo = { bar: "baz", baz: "super", foobar: "someOtherThing"};
 
